e6388c70461877cfecc3edcc0f7e059ddc369c38
Andrew KIl <kil.andrew@gmail.com>
2019-03-15 16:30:10 -0400

    create css file for profile component, figure out logic for posted/favorites button & adding class to the selected button to give the button a background color

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import "./Profile.css";
+
 export default class Profile extends Component {
   state = {
-    users: ""
+    users: "",
+    selectedButton: "posted",
+    postedClassName: "",
+    favoritesClassName: ""
   };
+
   componentDidMount() {
     axios
       .get("/users")
@@ -13,10 +19,56 @@ export default class Profile extends Component {
       })
       .catch(err => console.log(err));
   }
+
+  selectedButton = e => {
+    const { selectedButton } = this.state;
+    console.log(e.target.name);
+    // selectedButton === "posted"
+    //   ? this.setState({ postedClassName: "selected" })
+    //   : this.setState({
+    //       favoritesClassName: "selected",
+    //       selectedButton: "favorites"
+    //     });
+
+    if (e.target.name === "posted-button") {
+      this.setState({
+        postedClassName: "selected",
+        favoritesClassName: ""
+      });
+    } else {
+      this.setState({
+        favoritesClassName: "selected",
+        postedClassName: ""
+      });
+    }
+  };
+
   render() {
+    const { postedClassName, favoritesClassName } = this.state;
+
+    console.log(this.state);
+
     return (
       <>
         <h2>{this.state.users ? this.state.users[0].username : null}</h2>
+        <div id="profile-buttons">
+          <button
+            id="posted-button"
+            className={postedClassName}
+            onClick={this.selectedButton}
+            name="posted-button"
+          >
+            Posted
+          </button>
+          <button
+            id="favorites-button"
+            className={favoritesClassName}
+            onClick={this.selectedButton}
+            name="favorites-button"
+          >
+            Favorites
+          </button>
+        </div>
       </>
     );
   }

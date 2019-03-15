2fcd23825b8aba4fd274d3448810cb3335b80888
Andrew KIl <kil.andrew@gmail.com>
2019-03-15 11:25:35 -0400

    setup route for favorites

---
+++ routes/favorites.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+
+const {
+  getAllFavorites,
+  getAllFavoritesForSong,
+  getAllFavoritesForUser,
+  createFavorite,
+  deleteFavorite
+} = require("../db/queries/favoritesQueries.js");
+
+router.get("/", getAllFavorites);
+router.get("/song/:id", getAllFavoritesForSong);
+router.get("/user/:id", getAllFavoritesForUser);
+router.post("/", createFavorite);
+router.delete("/:id", deleteFavorite);
+
+module.exports = router;

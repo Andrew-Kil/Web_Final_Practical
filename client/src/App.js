25e953bc1fbc5ecf5099ad0c40bce22c8a874774
Andrew KIl <kil.andrew@gmail.com>
2019-03-15 11:46:21 -0400

    install react-router, react-router-dom, axios on frontend. install nodemon on backend. setup routing and navbar on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/songs">All Songs</NavLink>
+          <NavLink to="/popular">By Popularity</NavLink>
+          <NavLink to="/genre">By Genre</NavLink>
+          <NavLink to="/profile">My Profile</NavLink>
+        </nav>
+        Hi
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
       </div>
     );
   }
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div id="home-homepage">
+      <h1 id="home-header">Earworm Report</h1>
+    </div>
+  );
+}

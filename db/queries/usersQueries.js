fa6423720fff684ce0e367e4370887eb0e04ce5d
Andrew KIl <kil.andrew@gmail.com>
2019-03-15 10:42:42 -0400

    create user queries, setup index.js

---
+++ db/queries/usersQueries.js
@@ -0,0 +1,59 @@
+const { db } = require("./index.js");
+
+const getAllUsers = (req, res, next) => {
+  db.any("SELECT * FROM users")
+    .then(data => {
+      res.status(200).json({
+        status: "Success",
+        data: data,
+        message: "Received all users"
+      });
+    })
+    .catch(err => next(err));
+};
+
+const getSingleUser = (req, res, next) => {
+  const userId = Number(req.params.id);
+  db.one("SELECT * FROM users WHERE id=$1", userId)
+    .then(data => {
+      res.status(200).json({
+        status: "Success",
+        data: data,
+        message: "Received one user"
+      });
+    })
+    .catch(err => next(err));
+};
+
+const createUser = (req, res, next) => {
+  db.none("INSERT INTO users(username) VALUES(${username})", {
+    username: req.body.username
+  })
+    .then(() => {
+      res.status(200).json({
+        status: "Success",
+        message: "Created new user"
+      });
+    })
+    .catch(err => next(err));
+};
+
+const deleteUser = (req, res, next) => {
+  const userId = Number(req.params.id);
+  db.result("DELETE FROM users WHERE id=$1", userId)
+    .then(result => {
+      res.status(200).json({
+        status: "Success",
+        message: "Removed a user",
+        result: result
+      });
+    })
+    .catch(err => next(err));
+};
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  deleteUser
+};

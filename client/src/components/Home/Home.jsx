a442199c22175628187777873666805b670ceea8
Andrew KIl <kil.andrew@gmail.com>
2019-03-18 09:35:17 -0400

    modify CSS to meet requirements, work in progress

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Home.css";
+import music from "./music.jpg";
 
 export default function Home() {
   return (
     <div id="home-homepage">
       <h1 id="home-header">Earworm Report</h1>
+      <img
+        src={music}
+        alt="music background, headphones"
+        id="music-background"
+      />
     </div>
   );
 }
